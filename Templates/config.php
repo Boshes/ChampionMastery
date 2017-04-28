890263ff02006164df903e4474cd352bd44da240
Justen Morgan <justenamorgan@gmail.com>
2017-04-28 19:10:27 -0400

    updated for riot api v3

--- Templates/config.php
+++ Templates/config.php
@@ -4,11 +4,11 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: GET, POST, DELETE, PUT");
 header("Content-Type: application/json; charset=UTF-8");
 header("Connection: keep-alive");
-$apikey = 'RGAPI-65280E50-5D1F-4D46-BEBF-B4E9CC3904B3';
+$apikey = 'RGAPI-ff5fb4df-f6ed-4e04-8da4-3a2c4e565cd2';
 $summoner = $_REQUEST["summoner"];
 $summonerDetails = explode("_",$summoner);
 if($summoner!=''){
-  $url = 'https://'.$summonerDetails[1].'.api.pvp.net/api/lol/'.$summonerDetails[1].'/v1.4/summoner/by-name/'.$summonerDetails[0].'?api_key='.$apikey;
+  $url = 'https://'.$summonerDetails[2].'.api.riotgames.com/lol/summoner/v3/summoners/by-name/'.$summonerDetails[0].'?api_key='.$apikey;
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
@@ -16,8 +16,8 @@ if($summoner!=''){
   curl_close($ch);
   $summonerName = str_replace(' ','',strtolower($summonerDetails[0]));
   $information = json_decode($data,true);
-  $summonerId = $information[$summonerName]['id'];
-  $url = 'https://'.$summonerDetails[1].'.api.pvp.net/championmastery/location/'.$summonerDetails[2].'/player/'.$summonerId.'/champions?api_key='.$apikey;
+  $summonerId = $information['id'];
+  $url = 'https://'.$summonerDetails[2].'.api.riotgames.com/championmastery/location/'.$summonerDetails[2].'/player/'.$summonerId.'/champions?api_key='.$apikey;
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,false);
